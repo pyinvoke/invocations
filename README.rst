e10af3f16711748d8ec72644becdcf02f80a8163
Jeff Forcier <jeff@bitprophet.org>
2012-12-17 16:05:29 -0800

    Initial project setup

---
+++ README.rst
@@ -0,0 +1,12 @@
+
+.. image:: https://secure.travis-ci.org/pyinvoke/invocations.png?branch=master
+
+Invocations is a collection of reusable `http://pyinvoke.org <Invoke>`_
+tasks/task modules, including (but not limited to) Python project management
+tools such as documentation building and dependency organization.
+
+It has no stand-alone components and is designed to be imported into your
+pre-existing Invoke task files.
+
+Invocations is currently in pre-alpha status and is unsupported. Please follow
+the Invoke project's communication channels for updates. Thanks!
---
+++ setup.py
@@ -0,0 +1,47 @@
+#!/usr/bin/env python
+
+# Support setuptools or distutils
+try:
+    from setuptools import setup, find_packages
+except ImportError:
+    from distutils.core import setup
+
+# Version info -- read without importing
+_locals = {}
+version_module = execfile('invocations/_version.py', _locals)
+version = _locals['__version__']
+
+setup(
+    name='invocations',
+    version=version,
+    description='Reusable Invoke tasks',
+    license='BSD',
+    author='Jeff Forcier',
+    author_email='jeff@bitprophet.org',
+    url='http://pyinvoke.org',
+
+    # Release requirements. See dev-requirements.txt for dev version reqs.
+    requirements=['invoke>=0.1.0'],
+
+    packages=find_packages(),
+
+    classifiers=[
+          'Development Status :: 3 - Alpha',
+          'Environment :: Console',
+          'Intended Audience :: Developers',
+          'Intended Audience :: System Administrators',
+          'License :: OSI Approved :: BSD License',
+          'Operating System :: MacOS :: MacOS X',
+          'Operating System :: Unix',
+          'Operating System :: POSIX',
+          'Programming Language :: Python',
+          'Programming Language :: Python :: 2.6',
+          'Programming Language :: Python :: 2.7',
+          'Topic :: Software Development',
+          'Topic :: Software Development :: Build Tools',
+          'Topic :: Software Development :: Libraries',
+          'Topic :: Software Development :: Libraries :: Python Modules',
+          'Topic :: System :: Software Distribution',
+          'Topic :: System :: Systems Administration',
+    ],
+)
