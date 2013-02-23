2b9e6778098992d655c1b3665f1a8954800d8c29
Jeff Forcier <jeff@bitprophet.org>
2013-02-23 15:32:55 -0800

    Screw apidoc :(

--- invocations/docs.py
+++ invocations/docs.py
@@ -19,30 +19,7 @@ def browse_docs():
 
 
 @task
-def api_docs(target, output="api", exclude=""):
-    """
-    Runs ``sphinx-apidoc`` to autogenerate your API docs.
-
-    Must give target directory/package as ``target``. Results are written out
-    to ``docs/<output>`` (``docs/api`` by default).
-
-    To exclude certain output files from the final build give ``exclude`` as a
-    comma separated list of file paths.
-    """
-    output = os.path.join('docs', output)
-    # Have to make these absolute or apidoc is dumb :(
-    exclude = map(
-        lambda x: os.path.abspath(os.path.join(os.getcwd(), x)),
-        exclude.split(',')
-    )
-    run("sphinx-apidoc -f -o %s %s %s" % (output, target, ' '.join(exclude)))
-
-
-@task
 def docs(clean=False, browse=False):
-    """
-    Build Sphinx docs, optionally ``clean``ing and/or ``browse``ing.
-    """
     if clean:
         clean_docs.body()
     run("sphinx-build %s %s" % (docs_dir, build), pty=True)
