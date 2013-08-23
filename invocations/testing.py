9cefd4d933590dcea28cf221e6c5706c81cac882
Jeff Forcier <jeff@bitprophet.org>
2013-08-23 10:26:01 -0700

    Move docstring arg bits into per-arg help

--- invocations/testing.py
+++ invocations/testing.py
@@ -1,15 +1,13 @@
 from invoke import ctask as task
 
 
-@task
+@task(help={
+    'module': "Just runs tests/STRING.py.",
+    'runner': "Use STRING to run tests instead of 'spec'."
+})
 def test(ctx, module=None, runner='spec'):
     """
     Run a Spec or Nose-powered internal test suite.
-
-    Say ``--module=foo``/``-m foo`` to just run ``tests/foo.py``.
-
-    Defaults to running the ``spec`` tool; may override by saying e.g.
-    ``runner='nosetests'``.
     """
     # Allow selecting specific submodule
     specific_module = " --tests=tests/%s.py" % module
