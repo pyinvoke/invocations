fa85cbfe499599e8a0d667f25acae7fedcc13fb2
Jeff Forcier <jeff@bitprophet.org>
2013-11-12 10:37:42 -0800

    Add flag passthrough for 'test'

--- invocations/testing.py
+++ invocations/testing.py
@@ -3,14 +3,17 @@ from invoke import ctask as task
 
 @task(help={
     'module': "Just runs tests/STRING.py.",
-    'runner': "Use STRING to run tests instead of 'spec'."
+    'runner': "Use STRING to run tests instead of 'spec'.",
+    'opts': "Extra flags for the test runner",
 })
-def test(ctx, module=None, runner='spec'):
+def test(ctx, module=None, runner='spec', opts=None):
     """
     Run a Spec or Nose-powered internal test suite.
     """
     # Allow selecting specific submodule
     specific_module = " --tests=tests/%s.py" % module
     args = (specific_module if module else "")
+    if opts:
+        args += " " + opts
     # Use pty so the spec/nose/Python process buffers "correctly"
     ctx.run(runner + args, pty=True)
