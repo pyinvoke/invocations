62c775618693c2c9c970e7b21c3a31af67970ba0
Jeff Forcier <jeff@bitprophet.org>
2013-11-21 14:59:25 -0800

    Bump invoke version requirement re: config junk

--- setup.py
+++ setup.py
@@ -22,7 +22,7 @@ setup(
     url='http://pyinvoke.org',
 
     # Release requirements. See dev-requirements.txt for dev version reqs.
-    requirements=['invoke>=0.3.0'],
+    requirements=['invoke>=0.6.0'],
 
     packages=find_packages(),
 
