6530c51c00f0ce326a7fd4d3d24bbafd7b69dd20
Jeff Forcier <jeff@bitprophet.org>
2016-08-22 17:26:53 -0700

    Meh

--- invocations/packaging/__init__.py
+++ invocations/packaging/__init__.py
@@ -1,8 +1,9 @@
-# Just make the inner modules' tasks/collections readily available.
-# TODO: maaaaybe add our own sub-collection at this level so folks who want to
-# call stuff as 'packaging.vendorize' or 'packaging.release.upload' (vs binding
-# closer to project's root) have an easy import?
+# Make the inner modules' tasks/collections readily available.
 from .vendorize import vendorize
 from .release import release
+
+# Most of the time, importers of this module want the 'release' sub-collection.
+# TODO: update other libs & then remove this so it's a bit cleaner.
+ns = release
 
 # flake8: noqa
