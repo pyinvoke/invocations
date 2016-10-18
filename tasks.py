dae9795ac39d9d1d6c53df3d9f8582408c943a39
Jeff Forcier <jeff@bitprophet.org>
2016-10-17 18:11:32 -0700

    Boilerplatey bits for having tests

--- tasks.py
+++ tasks.py
@@ -1,10 +1,14 @@
 from invoke import Collection
 
 from invocations.packaging import release
+from invocations.testing import test, watch_tests
 
 
-ns = Collection(release)
+ns = Collection(release, test, watch_tests)
 ns.configure({
+    'tests': {
+        'package': 'invocations',
+    },
     'packaging': {
         'sign': True,
         'wheel': True,
