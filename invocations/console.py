054bd5d768413cdd4c160c210058fd2d45ac6486
Jeff Forcier <jeff@bitprophet.org>
2016-10-31 18:24:55 -0700

    Port confirm() from Fabric 1, and test the heck out of it

---
+++ invocations/console.py
@@ -0,0 +1,59 @@
+"""
+Text console UI helpers and patterns, e.g. 'Y/n' prompts and the like.
+"""
+
+from __future__ import unicode_literals, print_function
+
+import sys
+
+from invoke.vendor.six.moves import input
+
+
+# NOTE: originally cribbed from fab 1's contrib.console.confirm
+def confirm(question, affirmative=True):
+    """
+    Ask user a yes/no question and return their response as a boolean.
+
+    ``question`` should be a simple, grammatically complete question such as
+    "Do you wish to continue?", and will have a string similar to ``" [Y/n] "``
+    appended automatically. This function will *not* append a question mark for
+    you.
+
+    By default, when the user presses Enter without typing anything, "yes" is
+    assumed. This can be changed by specifying ``affirmative=False``.
+
+    .. note::
+        If the user does not supplies input that is (case-insensitively) equal
+        to "y", "yes", "n" or "no", they will be re-prompted until they do.
+
+    :param unicode question: The question part of the prompt.
+    :param bool affirmative:
+        Whether to assume the affirmative answer by default. Default value:
+        ``True``.
+
+    :returns: A `bool`.
+    """
+    # Set up suffix
+    if affirmative:
+        suffix = "Y/n"
+    else:
+        suffix = "y/N"
+    # Loop till we get something we like
+    # TODO: maybe don't do this? It can be annoying. Turn into 'q'-for-quit?
+    while True:
+        # TODO: ensure that this is Ctrl-C friendly, ISTR issues with
+        # raw_input/input on some Python versions blocking KeyboardInterrupt.
+        response = input("{0} [{1}] ".format(question, suffix))
+        response = response.lower() # Normalize
+        # Default
+        if not response:
+            return affirmative
+        # Yes
+        if response in ['y', 'yes']:
+            return True
+        # No
+        if response in ['n', 'no']:
+            return False
+        # Didn't get empty, yes or no, so complain and loop
+        err = "I didn't understand you. Please specify '(y)es' or '(n)o'."
+        print(err, file=sys.stderr)
---
+++ tests/console.py
@@ -0,0 +1,48 @@
+from __future__ import unicode_literals
+
+import sys
+
+from mock import patch
+from spec import Spec, eq_, ok_, trap, assert_contains
+
+from invocations.console import confirm
+
+
+class confirm_(Spec):
+    @patch('invocations.console.input', return_value='yes')
+    def displays_question_with_yes_no_suffix(self, mock_input):
+        confirm("Are you sure?")
+        eq_(mock_input.call_args[0][0], "Are you sure? [Y/n] ")
+
+    @patch('invocations.console.input')
+    def returns_True_for_yeslike_responses(self, mock_input):
+        for value in ('y', 'Y', 'yes', 'YES', 'yES', 'Yes'):
+            mock_input.return_value = value
+            ok_(confirm("Meh") is True)
+
+    @patch('invocations.console.input')
+    def returns_False_for_nolike_responses(self, mock_input):
+        for value in ('n', 'N', 'no', 'NO', 'nO', 'No'):
+            mock_input.return_value = value
+            ok_(confirm("Meh") is False)
+
+    @trap
+    @patch('invocations.console.input', side_effect=['wat', 'y'])
+    def reprompts_on_bad_input(self, mock_input):
+        ok_(confirm("O rly?") is True)
+        assert_contains(sys.stderr.getvalue(), "I didn't understand you")
+
+    @patch('invocations.console.input', return_value='y')
+    def suffix_changes_when_affirmative_False(self, mock_input):
+        confirm("Are you sure?", affirmative=False)
+        eq_(mock_input.call_args[0][0], "Are you sure? [y/N] ")
+
+    @patch('invocations.console.input', return_value='')
+    def default_on_empty_response_is_True_by_default(self, mock_input):
+        ok_(confirm("Are you sure?") is True)
+
+    @patch('invocations.console.input', return_value='')
+    def default_on_empty_response_is_False_if_affirmative_False(
+        self, mock_input
+    ):
+        ok_(confirm("Are you sure?", affirmative=False) is False)
