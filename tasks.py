4a8c6c40f4008a81e91fe5dbf6fce0531ee4b87e
Jeff Forcier <jeff@bitprophet.org>
2016-12-05 11:37:31 -0800

    Add basic coverage support for self

--- tasks.py
+++ tasks.py
@@ -1,10 +1,22 @@
-from invoke import Collection
+from invoke import Collection, task
 
 from invocations.packaging import release
-from invocations.testing import test, watch_tests
+from invocations.testing import test, watch_tests, coverage as coverage_
 
 
-ns = Collection(release, test, watch_tests)
+# TODO: add coverage at the Travis level as well sometime. For now this is just
+# to help me as I overhaul the release modules
+
+@task
+def coverage(c, html=True):
+    # TODO: can we realistically make use of functools.partial for this sort of
+    # thing?
+    # TODO: is it best left to config option overrides (currently the usual
+    # approach)? Is there stuff we can do to make that even easier?
+    return coverage_(c, html=html, integration_=False)
+
+
+ns = Collection(release, test, watch_tests, coverage)
 ns.configure({
     'tests': {
         'package': 'invocations',
