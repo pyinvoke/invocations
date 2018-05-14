4e623c9f0c4a6a2aa2491b86482e958369c93715
Jeff Forcier <jeff@bitprophet.org>
2018-05-14 16:18:11 -0400

    Grump

--- invocations/checks.py
+++ invocations/checks.py
@@ -11,6 +11,10 @@ from invoke import task
 def blacken(c, line_length=79, folder=None, check=False):
     """
     Run black on the current source tree (all ``.py`` files).
+
+    .. warning::
+        ``black`` only runs on Python 3.6 or above. (However, it can be
+        executed against Python 2 compatible code.)
     """
     default_folders = ["."]
     configured_folders = c.config.get("blacken", {}).get(
