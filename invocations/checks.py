e87ac65b42b6390cee835deb180fc6cd2a814082
Jeff Forcier <jeff@bitprophet.org>
2018-05-23 09:55:08 -0700

    Add diff option to blacken and document params

--- invocations/checks.py
+++ invocations/checks.py
@@ -8,13 +8,24 @@ from invoke import task
 
 
 @task(name="blacken", iterable=["folder"])
-def blacken(c, line_length=79, folder=None, check=False):
+def blacken(c, line_length=79, folder=None, check=False, diff=False):
     """
     Run black on the current source tree (all ``.py`` files).
 
     .. warning::
         ``black`` only runs on Python 3.6 or above. (However, it can be
         executed against Python 2 compatible code.)
+
+    :param int line_length:
+        Line length argument. Default: ``79``.
+    :param str folder:
+        Folder(s) to search within for ``.py`` files. May be given multiple
+        times to search N folders. Default: ``["."]``. Honors the
+        ``blacken.folders`` config option.
+    :param bool check:
+        Whether to run ``black --check``. Default: ``False``.
+    :param bool diff:
+        Whether to run ``black --diff``. Default: ``False``.
     """
     default_folders = ["."]
     configured_folders = c.config.get("blacken", {}).get(
@@ -25,6 +36,8 @@ def blacken(c, line_length=79, folder=None, check=False):
     black_command_line = "black -l {}".format(line_length)
     if check:
         black_command_line = "{} --check".format(black_command_line)
+    if diff:
+        black_command_line = "{} --diff".format(black_command_line)
 
     cmd = "find {} -name '*.py' | xargs {}".format(
         " ".join(folders), black_command_line
