c764b805d72b0d5d40392eca06c1175dddd4902f
Jeff Forcier <jeff@bitprophet.org>
2018-06-26 12:18:37 -0700

    Test existing configuration hooks

--- tests/checks.py
+++ tests/checks.py
@@ -52,3 +52,15 @@ class checks:
         def runs_black(self, ctx, kwargs, command):
             blacken(ctx, **kwargs)
             ctx.run.assert_called_once_with(command, pty=True)
+
+        def folder_configurable(self, ctx):
+            # Just config -> works fine
+            ctx.blacken = dict(folders=["elsewhere"])
+            blacken(ctx)
+            assert "elsewhere" in ctx.run.call_args[0][0]
+
+        def folder_config_loses_to_runtime(self, ctx):
+            # Config + CLI opt -> CLI opt wins
+            ctx.blacken = dict(folders=["nowhere"])
+            blacken(ctx, folder=["nowhere"])
+            assert "nowhere" in ctx.run.call_args[0][0]
