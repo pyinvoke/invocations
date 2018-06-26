21078cbdc329aab2add29501a84dc137ab52ec05
Jeff Forcier <jeff@bitprophet.org>
2018-06-25 18:07:25 -0700

    Flesh out rest of extant blacken behavior tests

--- tests/checks.py
+++ tests/checks.py
@@ -13,17 +13,42 @@ class checks:
                 "find . -name '*.py' | xargs black -l 79", pty=True
             )
 
-        def check_flag_passed_through(self):
-            skip()
+        def line_length_configurable(self, ctx):
+            blacken(ctx, line_length=80)
+            ctx.run.assert_called_once_with(
+                "find . -name '*.py' | xargs black -l 80", pty=True
+            )
 
-        def diff_flag_passed_through(self):
-            skip()
+        def target_folders_configurable(self, ctx):
+            blacken(ctx, folder=["foo", "bar"])
+            ctx.run.assert_called_once_with(
+                "find foo bar -name '*.py' | xargs black -l 79", pty=True
+            )
 
-        def line_length_configurable(self):
-            skip()
+        def check_flag_passed_through(self, ctx):
+            blacken(ctx, check=True)
+            ctx.run.assert_called_once_with(
+                "find . -name '*.py' | xargs black -l 79 --check", pty=True
+            )
 
-        def target_folders_configurable(self):
-            skip()
+        def diff_flag_passed_through(self, ctx):
+            blacken(ctx, diff=True)
+            ctx.run.assert_called_once_with(
+                "find . -name '*.py' | xargs black -l 79 --diff", pty=True
+            )
+
+        def multiple_options_given_simultaneously(self, ctx):
+            blacken(
+                ctx,
+                diff=True,
+                check=True,
+                line_length=80,
+                folder=["foo", "bar"],
+            )
+            ctx.run.assert_called_once_with(
+                "find foo bar -name '*.py' | xargs black -l 80 --check --diff",
+                pty=True,
+            )
 
         def subfolders_can_be_ignored(self):
             # TODO: how best to do this? there's no way at all to ignore
