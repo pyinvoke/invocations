29f002557651dbab3693feaf2110c288a352a4ee
Jeff Forcier <jeff@bitprophet.org>
2018-06-25 22:04:18 -0700

    Experimenting with pytest parameterization
    Pluses: saves some code over a loop+helper, including
    spaghetti calls with eg the ctx value
    Minuses: looks a little funny, and the output definitely
    looks pretty ugly

--- tests/checks.py
+++ tests/checks.py
@@ -1,4 +1,4 @@
-from pytest import skip
+import pytest
 
 from invocations.checks import blacken
 
@@ -7,53 +7,38 @@ class checks:
 
     class blacken_:
 
-        def base_case_is_all_files_and_79_characters(self, ctx):
-            blacken(ctx)
-            ctx.run.assert_called_once_with(
-                "find . -name '*.py' | xargs black -l 79", pty=True
-            )
-
-        def line_length_configurable(self, ctx):
-            blacken(ctx, line_length=80)
-            ctx.run.assert_called_once_with(
-                "find . -name '*.py' | xargs black -l 80", pty=True
-            )
-
-        def target_folders_configurable(self, ctx):
-            blacken(ctx, folder=["foo", "bar"])
-            ctx.run.assert_called_once_with(
-                "find foo bar -name '*.py' | xargs black -l 79", pty=True
-            )
-
-        def check_flag_passed_through(self, ctx):
-            blacken(ctx, check=True)
-            ctx.run.assert_called_once_with(
-                "find . -name '*.py' | xargs black -l 79 --check", pty=True
-            )
-
-        def diff_flag_passed_through(self, ctx):
-            blacken(ctx, diff=True)
-            ctx.run.assert_called_once_with(
-                "find . -name '*.py' | xargs black -l 79 --diff", pty=True
-            )
-
-        def multiple_options_given_simultaneously(self, ctx):
-            blacken(
-                ctx,
-                diff=True,
-                check=True,
-                line_length=80,
-                folder=["foo", "bar"],
-            )
-            ctx.run.assert_called_once_with(
-                "find foo bar -name '*.py' | xargs black -l 80 --check --diff",
-                pty=True,
-            )
-
-        def subfolders_can_be_ignored(self):
-            # TODO: how best to do this? there's no way at all to ignore
-            # vendor/ currently, even if you "opt in" you can't give the top
-            # level to get tasks.py, setup.py etc as it's recursive by default
-            # TODO: probably best to just make it a catchall opts passthru to
-            # find???
-            skip()
+        @pytest.mark.parametrize(
+            "kwargs,command",
+            [
+                (dict(), "find . -name '*.py' | xargs black -l 79"),
+                (
+                    dict(line_length=80),
+                    "find . -name '*.py' | xargs black -l 80",
+                ),
+                (
+                    dict(folder=["foo", "bar"]),
+                    "find foo bar -name '*.py' | xargs black -l 79",
+                ),
+                (
+                    dict(check=True),
+                    "find . -name '*.py' | xargs black -l 79 --check",
+                ),
+                (
+                    dict(diff=True),
+                    "find . -name '*.py' | xargs black -l 79 --diff",
+                ),
+                (
+                    dict(
+                        diff=True,
+                        check=True,
+                        line_length=80,
+                        folder=["foo", "bar"],
+                    ),
+                    "find foo bar -name '*.py' | xargs black -l 80 --check --diff",  # noqa
+                ),
+                # TODO: extra arbitrary 'find' opts passthru
+            ],
+        )
+        def runs_black(self, ctx, kwargs, command):
+            blacken(ctx, **kwargs)
+            ctx.run.assert_called_once_with(command, pty=True)
