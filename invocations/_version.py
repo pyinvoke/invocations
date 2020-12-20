482f2c2c987cecf3e49f7b5a0b7f0e326eb036cc
Jesse P. Johnson <jpj6652@gmail.com>
2020-12-20 10:41:20 -0500

    build: implement poetry

--- invocations/_version.py
+++ invocations/_version.py
@@ -1,2 +1,4 @@
-__version_info__ = (1, 4, 0)
-__version__ = ".".join(map(str, __version_info__))
+from importlib_metadata import version  # type: ignore
+
+__version__ = version(__package__)
+__version_info__ = tuple(map(int, __version__.split(".")))
