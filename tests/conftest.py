d9ae0e9929cedc85fc86b1d66721ea0d31cc0532
Jeff Forcier <jeff@bitprophet.org>
2021-03-19 16:04:45 -0400

    Test publish(), because ugh why wasn't it

--- tests/conftest.py
+++ tests/conftest.py
@@ -9,3 +9,15 @@ def ctx():
     # not happen" behavior
     MockContext.run_command = property(lambda self: self.run.call_args[0][0])
     return MockContext(run=True)
+
+
+# For use in packaging.release.publish tests
+@fixture
+def fakepub(mocker):
+    rmtree = mocker.patch("invocations.util.rmtree")
+    upload = mocker.patch("invocations.packaging.release.upload")
+    build = mocker.patch("invocations.packaging.release.build")
+    mkdtemp = mocker.patch("invocations.util.mkdtemp")
+    mkdtemp.return_value = "tmpdir"
+    c = MockContext(run=True)
+    yield c, mkdtemp, build, upload, rmtree
