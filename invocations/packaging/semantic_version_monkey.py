42890a6517e3e79fbc8a0a9f504af971b62fdf17
Jeff Forcier <jeff@bitprophet.org>
2025-07-27 23:58:59 -0400

    Mute some annoying SyntaxWarnings on modern Pythons

--- invocations/packaging/semantic_version_monkey.py
+++ invocations/packaging/semantic_version_monkey.py
@@ -5,6 +5,13 @@ We never like monkey-patching, but for now this is easier than either vendoring
 or distributing our own fork.
 """
 
+# Ignore SyntaxWarnings that come out of old semantic_version versions under
+# newer Pythons (re: comparing ints with 'is')
+# TODO: finish the WIP re: upgrading to modern semantic_version (it's
+# nontrivial unfortunately) then nuke this.
+from warnings import filterwarnings
+filterwarnings(action="ignore", category=SyntaxWarning, module=".*")
+
 
 from semantic_version import Version
 
